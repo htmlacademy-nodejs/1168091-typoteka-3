b438be0fd527efe0b8c5e33d86744cfd86953aa2
Konstantin <kandruk@ya.ru>
2021-04-01 16:05:08 +0300

    module3-task1

---
+++ src/express/express.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const DEFAULT_PORT = 8080;
+
+const express = require(`express`);
+const mainRoutes = require(`./routes/main-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
+
+const app = express();
+
+app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
+
+
+app.listen(DEFAULT_PORT);
