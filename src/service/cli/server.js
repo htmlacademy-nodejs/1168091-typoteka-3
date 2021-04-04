4480136d958ad5989cf81c1610454d77a9933e29
Konstantin <kandruk@ya.ru>
2021-04-04 14:29:56 +0300

    fix1

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -5,17 +5,21 @@ const router = require(`../routes/router`);
 const express = require(`express`);
 const chalk = require(`chalk`);
 
-const app = express();
-app.use(express.json());
-app.use(router);
+const startServer = (port) => {
+  const app = express();
+  app.use(express.json());
+  app.use(router);
+  app.listen(
+      port,
+      () => chalk.green(console.log(`The api server is running on port: ${port}`))
+  );
+};
 
 module.exports = {
   name: `--server`,
   run(args) {
     const [count] = args;
     const port = Number.parseInt(count, 10) || DEFAULT_PORT;
-    app.listen(
-        port,
-        () => chalk.green(console.log(`The api server is running on port: ${port}`)));
+    startServer(port);
   }
 };
