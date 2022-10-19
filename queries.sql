d59ae0a8f14ac4b7bfc3dbf6adbd5c2c692d9fc3
Constantine <kandruk@ya.ru>
2022-10-19 09:32:20 +0700

    added one request

--- queries.sql
+++ queries.sql
@@ -1,1 +1,10 @@
+-- список всех категорий
 SELECT * FROM categories
+
+-- список категорий где есть минимум одна публикация
+SELECT id, name FROM categories
+  JOIN articles_categories
+  ON id = categories_id
+  GROUP BY id
+
+--  список категорий с количеством публикаций
